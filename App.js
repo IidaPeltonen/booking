0f4c291e7ccfe58869da76a7ca02eca61f544981
IidaPeltonen <n0peii00@students.oamk.fi>
2022-10-07 12:25:39 +0300

    valmis

--- App.js
+++ App.js
@@ -1,13 +1,173 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Platform, StyleSheet, Text, View, Button } from 'react-native'
+import DateTimePicker from '@react-native-community/datetimepicker'
+import { Col, Grid } from 'react-native-easy-grid'
+import { useState } from 'react'
 
-export default function App() {
+export default function App () {
+  const [date, setDate] = useState(new Date())
+  const [startDate, setStartDate] = useState('')
+  const [endDate, setEndDate] = useState('')
+  const [startTime, setStartTime] = useState('')
+  const [endTime, setEndTime] = useState('')
+  const [status, setStatus] = useState('Pick start date and time')
+  const [mode, setMode] = useState('date')
+  const [show, setShow] = useState(false)
+
+  const showMode = currentMode => {
+    setShow(true)
+    setMode(currentMode)
+  }
+
+  const showDatePicker = () => {
+    showMode('date')
+  }
+
+  const showTimePicker = () => {
+    showMode('time')
+  }
+
+  function isValidEndDay (newDate) {
+    let start = startDate.split('.')
+    let end = newDate.split('.')
+    if (parseInt(end[2]) < parseInt(start[2])) {
+      return false
+    } else if (
+      parseInt(end[2]) === parseInt(start[2]) &&
+      parseInt(end[1]) < parseInt(start[1])
+    ) {
+      return false
+    } else if (
+      parseInt(end[2]) === parseInt(start[2]) &&
+      parseInt(end[1]) === parseInt(start[1]) &&
+      parseInt(end[1]) < parseInt(start[0])
+    ) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  function isValidEndTime (newTime) {
+    if (startDate === endDate) {
+      let start = startTime.split(':')
+      let end = newTime.split(':')
+      if (parseInt(end[0]) < parseInt(start[0])) {
+        return false
+      } else if (
+        parseInt(end[0]) === parseInt(start[0]) &&
+        parseInt(end[1]) <= parseInt(start[1])
+      ) {
+        return false
+      } else {
+        return true
+      }
+    } else {
+      return true
+    }
+  }
+
+  function handleDate (current) {
+    let newDate =
+      current.getDate() +
+      '.' +
+      (current.getMonth() + 1) +
+      '.' +
+      current.getFullYear()
+
+    if (startDate === '') {
+      setStartDate(newDate)
+    } else {
+      if (isValidEndDay(newDate)) {
+        setEndDate(newDate)
+      } else {
+        setStatus('Ends must be before starts')
+      }
+    }
+  }
+
+  function handleTime (current) {
+    let hours = current.getHours().toString()
+    let minutes = current.getMinutes().toString()
+
+    if (hours.length === 1) {
+      hours = '0' + hours
+    }
+    if (minutes.length === 1) {
+      minutes = '0' + minutes
+    }
+
+    let newTime = hours + ':' + minutes
+    if (startTime === '') {
+      setStartTime(newTime)
+      setStatus('Pick day and time')
+    } else {
+      if (isValidEndTime(newTime)) {
+        setEndTime(newTime)
+        setStatus('Well done!')
+      } else {
+        setStatus('Ends must be after starts')
+      }
+    }
+  }
+
+  const clearAll = () => {
+    setStartDate('')
+    setEndDate('')
+    setStartTime('')
+    setEndTime('')
+    setStatus('Pick start date and time')
+  }
+
+  const onChange = (e, selectedValue) => {
+    const current = selectedValue || date
+    setShow(Platform.OS === 'ios')
+    if (mode === 'date') {
+      handleDate(current)
+    } else {
+      handleTime(current)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Booking</Text>
+      <Text style={styles.time}>
+        Starts: {startDate} {startTime}
+      </Text>
+      <Text style={styles.time}>
+        Ends: {endDate} {endTime}
+      </Text>
+      <Text style={styles.time}> {status}</Text>
+      <Grid style={styles.grid}>
+        <Col size={30} style={styles.col}>
+          <View style={styles.picker}>
+            <Button onPress={showDatePicker} title='Pick date' />
+          </View>
+          {show && (
+            <DateTimePicker
+              testID='dateTimePicker'
+              value={date}
+              mode={mode}
+              is24Hour={true}
+              display='default'
+              onChange={onChange}
+            />
+          )}
+        </Col>
+        <Col size={30} style={styles.col}>
+          <View style={styles.picker}>
+            <Button onPress={showTimePicker} title='Pick time' />
+          </View>
+        </Col>
+        <Col size={30}>
+          <View style={styles.picker}>
+            <Button onPress={clearAll} title='Clear' />
+          </View>
+        </Col>
+      </Grid>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +175,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  header: {
+    marginTop: 50,
+    marginBottom: 10,
+    fontSize: 30
+  },
+  picker: {
+    marginTop: 30,
+    marginBottom: 30,
+    fontSize: 30
+  },
+  grid: {
+    marginLeft: 30,
+    marginRight: 30
+  },
+  col: {
+    marginLeft: 10,
+    marginRight: 10
+  },
+  time: {
+    marginTop: 10,
+    fontSize: 17
+  }
+})
